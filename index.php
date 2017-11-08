b2510a02af0cbf40a7587397be8301329b30de3e
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-08 11:50:58 -0700

    added audio files and audio folder

--- index.php
+++ index.php
@@ -10,5 +10,12 @@
 			<input type="button" onclick="incrementValue()" value="Pun Counter" />
 		</form>
 		<h1 id="punEncourage"></h1>
+		<div id="div1">The text above has been created dynamically.</div>
+
+		<audio>
+			<source src="audio/beep.mp3"></source>
+			<source src="audio/beep.ogg"></source>
+			Your browser isn't invited for super fun audio time.
+		</audio>
 	</body>
 </html>
