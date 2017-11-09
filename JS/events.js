f861f1fa58dc46dbabdbf44f4dea4ad49d1dc07f
Nicklas Pascetti <Npascetti@gmail.com>
2017-11-08 21:15:57 -0700

    added audio files

--- JS/events.js
+++ JS/events.js
@@ -1,43 +1,49 @@
 var value = parseInt(document.getElementById('number').value, 10);
 
-function incrementValue()
-{
+function incrementValue() {
+	var audio = document.getElementById("mySoundClip");
+	var audio1 = document.getElementById("mySoundClip1");
+	var audio2 = document.getElementById("mySoundClip2");
+	var audio3 = document.getElementById("mySoundClip3");
 	value = isNaN(value) ? 0 : value;
 	value++;
-		if (value < 10) {
-			document.getElementById('number').value = value + ' puns';
-		} else if (value >= 10 && value < 25) {
-			document.getElementById('number').value = value + ' puns';
-			document.getElementById('punEncourage').innerHTML = 'Keep those puns coming!';
-				if (value === 10) {
-					audio.play();
-				}
-		} else if (value >= 25 && value < 50) {
-			document.getElementById('number').value = value + ' puns';
-			document.getElementById('punEncourage').innerHTML = 'Holy shit that\'s alot of puns!';
-			audio1.play();
-		} else if (value >= 50) {
-			document.getElementById('number').value = value + ' puns';
+	if(value < 10) {
+		document.getElementById('number').value = value + ' puns';
+	} else if(value >= 10 && value < 25) {
+		document.getElementById('number').value = value + ' puns';
+		document.getElementById('punEncourage').innerHTML = 'Keep those puns coming!';
+		if(value === 10) {
+			audio.play();
 		}
-
+	} else if(value >= 25 && value < 50) {
+		document.getElementById('number').value = value + ' puns';
+		document.getElementById('punEncourage').innerHTML = 'Holy shit that\'s alot of puns!';
+		if(value === 25) {
+			audio1.play();
+		}
+	} else if(value >= 50) {
+		document.getElementById('number').value = value + ' puns';
+		document.getElementById('punEncourage').innerHTML = 'THIS IS INHUMAN! ARE YOU A PUN MACHINE?';
+		if(value === 50) {
+			audio2.play();
+		}
+	}
 }
+	document.body.onload = addElement;
 
-document.body.onload = addElement;
+	function addElement() {
+		// create a new div element
+		// and give it some content
+		var newDiv = document.createElement("div");
+		var newContent = document.createTextNode("Hi there and greetings!");
+		newDiv.appendChild(newContent); //add the text node to the newly created div.
 
-function addElement () {
-	// create a new div element
-	// and give it some content
-	var newDiv = document.createElement("div");
-	var newContent = document.createTextNode("Hi there and greetings!");
-	newDiv.appendChild(newContent); //add the text node to the newly created div.
+		// add the newly created element and its content into the DOM
+		var currentDiv = document.getElementById("div1");
+		document.body.insertBefore(newDiv, currentDiv);
+	}
 
-	// add the newly created element and its content into the DOM
-	var currentDiv = document.getElementById("div1");
-	document.body.insertBefore(newDiv, currentDiv);
-}
 
-var audio = document.getElementById("mySoundClip");
-var audio1 = document.getElementById("mySoundClip1");
 
 
 
--- index.php
+++ index.php
@@ -3,7 +3,19 @@
 	<head>
 		<meta charset="utf-8"/>
 		<script type="text/javascript" rel="script" src="JS/events.js" async></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<audio id="mySoundClip">
+			<source src="audio/cartoon-telephone_daniel_simion.mp3"></source>
+			Your browser isn't invited for super fun audio time.
+		</audio>
+		<audio id="mySoundClip1">
+			<source src="audio/front-desk-bells-daniel_simon.mp3"></source>
+		</audio>
+		<audio id="mySoundClip2">
+			<source src="audio/Airhorn-SoundBible.com-975027544.mp3"></source>
+		</audio>
+		<audio id="mySoundClip3">
+			<source src="audio/carelessairhorn.mp3"></source>
+		</audio>
 	</head>
 	<body>
 		<form>
@@ -15,13 +27,11 @@
 
 		<p id="clickp">try clicking on me</p>
 
-		<audio id="mySoundClip">
-			<source src="audio/cartoon-telephone_daniel_simion.mp3"></source>
-			Your browser isn't invited for super fun audio time.
-		</audio>
-		<audio id="mySoundClip1">
-			<source src="audio/front-desk-bells-daniel_simon.mp3"></source>
-		</audio>
+		<p onclick="replaceTaco()">Every day is taco ipsum tuesday. It’s a wonderful morning for breakfast tacos. It’s a wonderful morning for breakfast tacos. How do you feel about hard shelled tacos? Ooh, with diced onions and a pinch of cilantro. I think I’ve overdosed on tacos. Yeah, apparently the taco shack was robbed. They left the money but took the tacos. I’ve been following that taco truck around all day. 50 cent tacos! I’ll take 30. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Tacos dorados called flautas, or taquitos, for which the tortillas are filled with pre-cooked shredded chicken, beef or barbacoa, rolled into an elongated cylinder and deep-fried until crisp.</p>
+
+		<p>CARNITAS!! Give me all the tacos, immediately. Tacos, again? This will be 5 times this week and it’s only Tuesday. Give me all your tacos. Ooh, with diced onions and a pinch of cilantro. How do you feel about hard shelled tacos? Ooh, with diced onions and a pinch of cilantro. It’s a wonderful morning for breakfast tacos. Tacos al pastor made with adobada meat. Tacos, tacos, tacos. Shrimp tacos are tasty tacos! Black or pinto beans? Tacos Al pastor/De Adobada are made of thin pork steaks seasoned with adobo seasoning, then skewered and overlapped on one another on a vertical rotisserie cooked and flame-broiled as it spins.</p>
+
+		<p>It’s long been rumored that the chupacabra is really just a crazed man who’s local taco shop went out of business. If you were a taco, would you eat yourself? CARNE ASADA!! Give me all the tacos, immediately. BARBACOA!! How do you feel about hard shelled tacos? Burritos are very tasty. Give me all your tacos. Burritos are very tasty. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla.</p>
 
 	</body>
 </html>
